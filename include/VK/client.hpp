b5fc4605600cb5bb1be3fdf0d04dadc92c56d886
param42 <param42@ya.ru>
2016-10-26 19:51:37 +0300

    Update client.hpp

--- include/VK/client.hpp
+++ include/VK/client.hpp
@@ -1,1 +1,11 @@
+#include <string>
+#include <map>
 
+namespace Facebook{
+  class Client{
+  public:
+    using dict_t = std::map<std::string, std::string>;
+    Client(dict_t settings);
+    auto check_connection() -> bool;
+  };
+}
