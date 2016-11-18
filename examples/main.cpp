eb0081a3cde409937f2fd956b8f91cc713ac8cf4
param42 <param42@ya.ru>
2016-11-18 14:38:31 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,27 +1,30 @@
+ 
 
+#include "stdafx.h"
 #include <curl\curl.h>
 #include "client.h"
+#include <vector>
 
-
-std::string token = "522b383faa06ad0e50c5b1c5ebb70a8fe1a92d285b3d61b40e14604f5ffc4f3406d6c3dbe0c116477e09d";
+ 
 
 int main()
 {
-	
+	setlocale(LC_ALL, "Russian");
 	VK::Client cl({ {"token", token} });
 	
 	if (cl.check_connection()) {
-		std::cout << "yes";
+		std::vector<VK::User>users;
+			users=cl.get_frientd_online();
+
+
+			for (int i = 0; i < users.size(); i++) {
+				std::cout << users[i].id << std::endl;
+			}
+
 	}
-	else {
-		std::cout << "no";
-	}
-	 
+		
+
 		system("pause");
 		return 0;
 	}
-
-
-
-
 
