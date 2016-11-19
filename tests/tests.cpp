466b70b8b08cb2e528d2e227420df118e7570587
param42 <param42@ya.ru>
2016-11-19 20:33:51 +0300

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -23,7 +23,7 @@ SCENARIO("test")
 
     GIVEN("valid token")
     {
-        std::map<std::string, std::string> valid_settings{{"token", "8ff6ca92334fcaf564fff562320406f263276d16e6f41cdb0145d6b4a0dd6435c513af2c63986bf19bfa3"}};
+        std::map<std::string, std::string> valid_settings{{"token", "672f98cc5c7b4e740e51d9360ad1126bc4484599fd41290b4f9759b74003ff23e545c521b652d73e9e93b"}};
 
         WHEN("initialize")
         {
@@ -37,7 +37,7 @@ SCENARIO("test")
     }
     GIVEN("GET_FRIEND_ONLINE")
     {
-     std::map<std::string, std::string> valid_settings{{"token", "522b383faa06ad0e50c5b1c5ebb70a8fe1a92d285b3d61b40e14604f5ffc4f3406d6c3dbe0c116477e09d"}};
+     std::map<std::string, std::string> valid_settings{{"token", "672f98cc5c7b4e740e51d9360ad1126bc4484599fd41290b4f9759b74003ff23e545c521b652d73e9e93b"}};
      WHEN("GET_FRIEND")
      {
         VK::Client client(valid_settings);
