ab48dadd171c470a15dffd1e63c79c0cd0240521
param42 <param42@ya.ru>
2016-11-24 19:34:34 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -14,27 +14,8 @@ std::string token = "127961f2d122ca42393fd8b9++++de04489e7cd785bb";
 
 int main()
 {
-	VK::Client cl({ { "token", token } });
-	std::vector<VK::User>users;
-	if (cl.check_connection()) {
-		 
-		users = cl.get_frientd_online();
-
-
-		for (int i = 0; i < users.size(); i++) {
-			std::cout << users[i].id << std::endl;
-		}
-		std::cout << "____" << std::endl;
-	}
-
-	//FRTThread ft(users);
 	
-	//	if (ft.cin()) {
-	//		std::cout << "all_threads_end" << std::endl;
-	//	}
-	//	else {
-	//		std::cout << "error";
-	//	}
+
 
 
 		system("pause");
