e104085864e63cf338387c30bcabcc4b82d88f43
param42 <param42@ya.ru>
2016-11-25 23:38:54 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -8,16 +8,25 @@
 #include <future>
 #include <chrono>
 #include "VK/FRTThread.hpp"
-std::string token = "127961f2d122ca42393fd8b9++++de04489e7cd785bb";
 
  
 
 int main()
-{
 	
+{	std::string code;
+    	std::cout << "Для получения code пройдите по данному url:" << std::endl;
+   	std::cout << "https://oauth.vk.com/authorize?client_id=5454862&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=code&v=5.60" << std::endl;
+    	std::cout << "code = ";
+    	std::cin >> code;
+	VK::Client client("code",code);
 
-
-
+ 		If( Client::check_connection_server()){
+	 
+		FRTThread threads_friends(client.get_frientd_online());
+			
+		 threads_friends.cin();
+			
+		}
 		system("pause");
 		return 0;
 	}
