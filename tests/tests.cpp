25f96dfebf471be98103028c8c82e6e20ad67e18
param42 <param42@ya.ru>
2016-11-26 16:11:56 +0300

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -8,7 +8,7 @@ SCENARIO("test")
 {
  
  std::string invalid_code="";
- std::string valid_code="704431a2e75ff1ae5b";
+ std::string valid_code="6eddfc965034236238";
  
  VK::Client invalide_client({{"code", invalid_code}});
  VK::Client valide_client({{"code", valid_code}});
