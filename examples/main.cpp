e5d5a5dc777bf6b5721dcd018563ea3d7ea75043
param42 <param42@ya.ru>
2016-11-26 14:50:49 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -14,19 +14,19 @@
 int main()
 	
 {	std::string code;
-    	std::cout << "Для получения code пройдите по данному url:" << std::endl;
-   	std::cout << "https://oauth.vk.com/authorize?client_id=5454862&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=code&v=5.60" << std::endl;
-    	std::cout << "code = ";
-    	std::cin >> code;
-	VK::Client client("code",code);
+	std::cout << "Для получения code пройдите по данному url:" << std::endl;
+	std::cout << "https://oauth.vk.com/authorize?client_id=5454862&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=code&v=5.60" << std::endl;
+	std::cout << "code = ";
+	std::cin >> code;
+	VK::Client client({ { "code", code } });
 
- 		If( Client::check_connection_server()){
-	 
+	if(client.check_connection_server()) {
+
 		FRTThread threads_friends(client.get_frientd_online());
-			
-		 threads_friends.cin();
-			
-		}
+
+		threads_friends.cin();
+
+	}
 		system("pause");
 		return 0;
 	}
